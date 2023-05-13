f49b444486895423a1d77bf4e28b89742da53515
reham <rhrahoma@gmail.com>
2023-05-13 15:48:10 +0300

    update

--- flutter_app_2drety/lib/Pages/page2.dart
+++ flutter_app_2drety/lib/Pages/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_2drety/Pages/page3.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 import '../componant/com.dart';
@@ -22,7 +23,7 @@ class _OrderResturantState extends State<OrderResturant> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
+     
     return Scaffold(
       body: Column(
         children: [
@@ -30,11 +31,25 @@ class _OrderResturantState extends State<OrderResturant> {
             clipper: Myclipp(),
             child: Stack(children: [
               Container(
-                color: Color(0XFFFff3939),
+                color: const Color(0xfffff3939),
                 height: size.height / 4,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 50),
+               Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 25),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 50),
                 child: Center(
                     child: Icon(
                   Icons.person,
@@ -44,13 +59,89 @@ class _OrderResturantState extends State<OrderResturant> {
               ),
             ]),
           ),
+          Container(
+            height: size.height / 11,
+          ),
+          Stack(children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return  OrderMenu();
+              }));
+              }, // Handle your callback.
+
+              splashColor: Colors.brown.withOpacity(0.5),
+
+              child: Ink(
+                height: 110,
+                width: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: const DecorationImage(
+                    image: AssetImage('lib/assets/images/images (10).jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            //Container(color: Colors.greenAccent,width: 200,height: 110,),
+            const Text("سندوتشات",style: TextStyle(color: Colors.white,fontSize: 35,fontWeight: FontWeight.bold),),
+          ]),          Container(height: size.height/35,),
+
+           Stack(children: [
+            InkWell(
+              onTap: () { Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return  OrderMenu();
+              }));}, // Handle your callback.
+
+              splashColor: Colors.brown.withOpacity(0.5),
+
+              child: Ink(
+                height: 110,
+                width: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: const DecorationImage(
+                    image: AssetImage('lib/assets/images/images (1).jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            //Container(color: Colors.greenAccent,width: 200,height: 110,),
+            const Text("سايب",style: TextStyle(color: Colors.white,fontSize: 35,fontWeight: FontWeight.bold),),
+          ]),
+          Container(height: size.height/35,),
+           Stack(children: [
+            InkWell(
+              onTap: () { Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return  OrderMenu();
+              }));}, // Handle your callback.
+
+              splashColor: Colors.brown.withOpacity(0.5),
+
+              child: Ink(
+                height: 110,
+                width: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: const DecorationImage(
+                    image: AssetImage('lib/assets/images/images (23).jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            //Container(color: Colors.greenAccent,width: 200,height: 110,),
+            const Text("طلب",style: TextStyle(color: Colors.white,fontSize: 35,fontWeight: FontWeight.bold),),
+          ])
         ],
       ),
       bottomNavigationBar: SnakeNavigationBar.color(
         //height: 110,
         selectedItemColor: Colors.white,
-        snakeViewColor: Color(0XFFFff3939),
-        unselectedItemColor: Color(0XFFFff3939),
+        snakeViewColor: const Color(0xfffff3939),
+        unselectedItemColor: const Color(0xfffff3939),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
---
+++ flutter_app_2drety/lib/login/Cubit/login_cubit/login_state.dart
@@ -0,0 +1,18 @@
+part of 'login_cubit.dart';
+
+@immutable
+abstract class LoginState {}
+
+class LoginInitial extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginFailure extends LoginState {
+  String errmassage;
+  LoginFailure({
+    required this.errmassage,
+  });
+  
+}
